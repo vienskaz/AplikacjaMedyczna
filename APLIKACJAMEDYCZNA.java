6a1a5618da91ae44e24df1ce02e750e4a27a02c4
Adam <adwiesek@wp.pl>
2023-08-08 18:52:32 +0200

    having trouble with adding name and surname

--- APLIKACJAMEDYCZNA.java
+++ APLIKACJAMEDYCZNA.java
@@ -6,51 +6,102 @@ public class APLIKACJAMEDYCZNA
     public static void main(String[] args) {
         
 
-        ArrayList<Patient> patients = new ArrayList<>();
+        ArrayList<Patient> patientsList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-       
+        menu(patientsList, scanner);
 
-        addPatients(patients, scanner);
+
+        
 
     }
-     public static void addPatients(ArrayList<Patient> patientsList, Scanner scanner)
+    public static void addPatients(ArrayList<Patient> patientsList, Scanner scanner) {
+        System.out.println("Name: ");
+        String name = scanner.nextLine();
+    
+        System.out.println("Surname: ");
+        String surname = scanner.nextLine();
+    
+        System.out.println("Age: ");
+        int age = scanner.nextInt();
+    
+        System.out.println("Weight in kg: ");
+        double weight = scanner.nextDouble();
+    
+        System.out.println("Height in cm: ");
+        double height = scanner.nextDouble();
+    
+        System.out.println("Temperature in \u00B0C: ");
+        double temperature = scanner.nextDouble();
+    
+        System.out.println("Pulse: ");
+        int pulse = scanner.nextInt();
+    
+        System.out.println("Systolic Pressure: ");
+        int systolicPressure = scanner.nextInt();
+    
+        Patient newPatient = new Patient(name, surname, age, weight, height, temperature, pulse, systolicPressure);
+        patientsList.add(newPatient);
+    
+        System.out.println("Patient added successfully!\n");
+        System.out.println(newPatient.getName());
+        System.out.println(newPatient.getSurname());
+        menu(patientsList, scanner);
+    }
+    
+
+
+        public static void menu(ArrayList<Patient> patientsList, Scanner scanner)
         {
-            System.out.println("Name: ");
-            String name = scanner.nextLine();
+            System.out.println("What do you want to do?");
+            System.out.println("To add patient enter 1");
+            System.out.println("To delete patient enter 2");
+            System.out.println("To check patient enter 3");
+            int select= scanner.nextInt();
+        
+        switch(select)
+        {
+            case 1:
+            addPatients(patientsList, scanner);
+            break;
+            
+            case 2:
+            boolean patients_check=patientsList.isEmpty();
+            if(patients_check==true)
+            {
+                System.out.println("You have not patients to delete.");
+                System.out.println("Do you want to add one?");
+                System.out.println("Enter 1 to add, enter 2 to quit");
+                int select_nested= scanner.nextInt();
+                switch(select_nested){
+                    case 1:
+                    addPatients(patientsList, scanner);
+                    break;
+                    case 2:
+                    menu(patientsList, scanner);
+                    break;
+                }
+            break;
 
-            System.out.println("Surname: ");
-            String surname = scanner.nextLine();
+            }
+            else {
+                System.out.println("Select patient to delete from the list");
+                for (int i = 0; i < patientsList.size(); i++) {
+                    Patient patient = patientsList.get(i);
+                    System.out.println(i + ": " + patient.getName() + " " + patient.getSurname());
+                }
+                int selectToDelete = scanner.nextInt();
+                patientsList.remove(selectToDelete);
+            }
+            
 
-            System.out.println("Is the patient a man? (true/false): ");
-            boolean isMan = scanner.nextBoolean();
-            scanner.nextLine();
 
-            System.out.println("Age: ");
-            int age = scanner.nextInt();
-            scanner.nextLine(); 
-
-            System.out.println("Weight: ");
-            double weight = scanner.nextDouble();
-            scanner.nextLine(); 
-
-            System.out.println("Height: ");
-            double height = scanner.nextDouble();
-            scanner.nextLine();
-
-            System.out.println("Temperature: ");
-            double temperature = scanner.nextDouble();
-            scanner.nextLine(); 
-            System.out.println("Pulse: ");
-            int pulse = scanner.nextInt();
-            scanner.nextLine(); 
-
-            System.out.println("Systolic Pressure: ");
-            int systolicPressure = scanner.nextInt();
-            scanner.nextLine();
-
-            Patient newPatient = new Patient(name, surname, isMan, age, weight, height, temperature, pulse, systolicPressure);
-            patientsList.add(newPatient);
-
-            System.out.println("Patient added successfully!\n");
+            default:
+            System.out.println("Select option");
+            menu(patientsList, scanner);
+            break;
         }
+        }
+
+
+
 }
--- Patient.java
+++ Patient.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
 
 public class Patient
 {
     String name;
     String surname;
-    boolean isMan;
     int age;
     double weight;
     double height;
@@ -13,12 +11,11 @@ public class Patient
     int systolicPressure;
     
     
-    Patient(String name,String surname,boolean  isMan,int age,double weight,double height,double temperature,int pulse,
+    Patient(String name,String surname,int age,double weight,double height,double temperature,int pulse,
     int systolicPressure)
     {
         this.name=name;
         this.surname=surname;
-        this.isMan=isMan;
         this.weight=weight;
         this.height=height;
         this.temperature=temperature;
@@ -36,10 +33,6 @@ public class Patient
         this.surname=surname;
     }
 
-    public void setisMan(boolean isMan)
-    {
-        this.isMan=isMan;
-    }
 
     public void setWeight(double weight)
     {
@@ -77,10 +70,6 @@ public class Patient
         return surname;
     }
 
-    public boolean getisMan()
-    {
-        return isMan;
-    }
 
     public double getWeight(double weight)
     {
@@ -92,7 +81,7 @@ public class Patient
         return height;
     }
 
-    public double getTemperature( )
+    public double getTemperature()
     {
         return temperature;
     }
@@ -113,8 +102,7 @@ public class Patient
         height=height/100;
         double BMI=weight/(height*height);
 
-        if(isMan==true)
-        {
+      
             if(BMI<18.5)
             {
                 System.out.println("This patient's BMI is "+BMI+" which stands for underweight");
@@ -131,29 +119,8 @@ public class Patient
             {
                 System.out.println("This patient's BMI is "+BMI+" which stands for obsiety");
             }
-        }
-        else{
-            if(BMI<18.5)
-            {
-                System.out.println("This patient's BMI is "+BMI+" which stands for underweight");
-            }
-            else if(BMI>=18.5 && BMI<=24.9 )
-            {
-                System.out.println("This patient's BMI is "+BMI+" which stands for healty weight");
-            }
-            else if(BMI>=25 && BMI<=29.9)
-            {
-                System.out.println("This patient's BMI is "+BMI+" which stands for overweight");
-            }
-            else
-            {
-                System.out.println("This patient's BMI is "+BMI+" which stands for obsiety");
-            }
-
-        }
-
         
-
+        
     }
 
 
